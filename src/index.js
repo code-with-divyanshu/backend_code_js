d4b43f3c93ab96722f654cd2f1311fc1989fdeb1
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-08-25 23:53:27 +0530

    Utils code custom api & Express Setup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({
   path: "/.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection Failed !! ", err);
+  });
 
 /* first approach
 (async () => {
